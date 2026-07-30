19b443348e91014de3457b30d0b93fe88427ef1b
Elian Meaca <elianmeaca@gmail.com>
2026-07-30 01:33:13 -0300

    How it works section and reference sections html

--- frontend/src/app/home/home.ts
+++ frontend/src/app/home/home.ts
@@ -2,10 +2,14 @@ import { Component } from '@angular/core';
 import { Hero } from "./hero/hero";
 import { Features } from './features/features';
 import { Security } from './security/security';
+import { DevReference } from './dev-reference/dev-reference';
+import { HowItWorks } from './how-it-works/how-it-works';
+import { FreelanceReference } from './freelance-reference/freelance-reference';
+import { CallToAction } from './call-to-action/call-to-action';
 
 @Component({
   selector: 'app-home',
-  imports: [Hero, Features, Security],
+  imports: [Hero, Features, Security, HowItWorks, DevReference, FreelanceReference, CallToAction],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
