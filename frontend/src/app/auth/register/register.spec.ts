9391e01cbf845bafbc9a90226aed3d9558b372fa
Elian Meaca <elianmeaca@gmail.com>
2026-08-03 15:01:35 -0300

    Finished home, generated remaining routes

--- frontend/src/app/auth/register/register.spec.ts
+++ frontend/src/app/auth/register/register.spec.ts
@@ -1,17 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { CallToAction } from './call-to-action';
+import { Register } from './register';
 
-describe('CallToAction', () => {
-  let component: CallToAction;
-  let fixture: ComponentFixture<CallToAction>;
+describe('Register', () => {
+  let component: Register;
+  let fixture: ComponentFixture<Register>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CallToAction],
+      imports: [Register],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(CallToAction);
+    fixture = TestBed.createComponent(Register);
     component = fixture.componentInstance;
     await fixture.whenStable();
   });
--- frontend/src/app/home/home.ts
+++ frontend/src/app/home/home.ts
@@ -5,11 +5,12 @@ import { Security } from './security/security';
 import { DevReference } from './dev-reference/dev-reference';
 import { HowItWorks } from './how-it-works/how-it-works';
 import { FreelanceReference } from './freelance-reference/freelance-reference';
-import { CallToAction } from './call-to-action/call-to-action';
+import { Footer } from './footer/footer';
+import { Header } from './header/header';
 
 @Component({
   selector: 'app-home',
-  imports: [Hero, Features, Security, HowItWorks, DevReference, FreelanceReference, CallToAction],
+  imports: [Hero, Features, Security, HowItWorks, DevReference, FreelanceReference, Footer, Header],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
